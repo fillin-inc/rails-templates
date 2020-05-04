0b8c08abc6e2b03d24395ae98fe616008d8a825f
Yuya Matsushima <yuyam@fillin-inc.com>
2020-05-04 13:43:29 +0900

    調整

--- rails/default.rb
+++ rails/default.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 gem 'enumerize'
+gem 'mysql2'
 gem 'redis-rails'
 gem 'slim-rails'
 
@@ -11,8 +14,8 @@ end
 
 gem_group :development do
   gem 'annotate'
-  gem "better_errors"
-  gem "binding_of_caller"
+  gem 'better_errors'
+  gem 'binding_of_caller'
   gem 'bullet'
   gem 'capistrano', require: false
   gem 'capistrano-rails', require: false
@@ -32,53 +35,64 @@ gsub_file 'Gemfile', /^gem\s+['']coffee-rails[''].*$/, ''
 gsub_file 'Gemfile', /^\s+gem\s+['']chromedriver-helper[''].*$/, ''
 
 initializer 'generators.rb', <<-CODE
-Rails.application.config.generators do |g|
-  g.assets false
-  g.factory_bot false
-  g.helper false
-  g.jbuilder false
-  g.template_engine = :slim
-  g.test_framework :rspec, controller_specs: false, view_specs: false, routing_specs: false, request_specs: false
-end
+  # frozen_string_literal: true
+
+  Rails.application.config.generators do |g|
+    g.assets false
+    g.factory_bot false
+    g.helper false
+    g.jbuilder false
+    g.template_engine = :slim
+    g.test_framework :rspec,
+                     controller_specs: false,
+                     view_specs: false,
+                     routing_specs: false
+  end
 CODE
 
 initializer 'bullet.rb', <<-CODE
-Rails.application.config.after_initialize do
-  Bullet.enable        = true
-  Bullet.bullet_logger = true
-  Bullet.console       = true
-  Bullet.rails_logger  = true
-end
+  # frozen_string_literal: true
+
+  if Rails.env.development?
+    Rails.application.config.after_initialize do
+      Bullet.enable        = true
+      Bullet.bullet_logger = true
+      Bullet.console       = true
+      Bullet.rails_logger  = true
+    end
+  end
 CODE
 
 initializer 'sass.rb', <<-CODE
-Rails.application.config.sass.preferred_syntax = :sass
+  # frozen_string_literal: true
+
+  Rails.application.config.sass.preferred_syntax = :sass
 CODE
 
 file '.rubocop.yml', <<-CODE
-require: rubocop-rails
+  require: rubocop-rails
 
-Rails/UnknownEnv:
-  Environments:
-    - production
-    - development
-    - test
-    - staging
+  Rails/UnknownEnv:
+    Environments:
+      - production
+      - development
+      - test
+      - staging
 
-Style/AsciiComments:
-  Enabled: false
+  Style/AsciiComments:
+    Enabled: false
 
-Style/ClassAndModuleChildren:
-  Enabled: false
+  Style/ClassAndModuleChildren:
+    Enabled: false
 
-Style/Documentation:
-  Enabled: false
+  Style/Documentation:
+    Enabled: false
 
-Metrics/LineLength:
-  Max: 120
+  Metrics/LineLength:
+    Max: 120
 
-Metrics/MethodLength:
-  Max: 20
+  Metrics/MethodLength:
+    Max: 20
 CODE
 
 after_bundle do
