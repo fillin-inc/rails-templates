27479437614cc4a070d15bc6531c3b1a01b343f9
Yuya Matsushima <yuyam@fillin-inc.com>
2020-05-04 15:18:25 +0900

    いろいろ追加

--- rails/default.rb
+++ rails/default.rb
@@ -32,7 +32,6 @@ gem_group :development do
 end
 
 gem_group :test do
-  gem 'database_cleaner'
   gem 'capybara'
   gem 'fakeredis'
   gem 'puma' # for capybara
@@ -49,7 +48,7 @@ initializer 'generators.rb', <<-CODE
 
   Rails.application.config.generators do |g|
     g.assets false
-    g.factory_bot false
+    g.factory_bot dir: 'spec/factories'
     g.helper false
     g.jbuilder false
     g.template_engine = :slim
@@ -66,9 +65,9 @@ initializer 'bullet.rb', <<-CODE
   if Rails.env.development?
     Rails.application.config.after_initialize do
       Bullet.enable        = true
-      Bullet.bullet_logger = true
       Bullet.console       = true
       Bullet.rails_logger  = true
+      Bullet.bullet_logger = false
     end
   end
 CODE
@@ -79,8 +78,34 @@ initializer 'sass.rb', <<-CODE
   Rails.application.config.sass.preferred_syntax = :sass
 CODE
 
+file 'config/locales/.keep'
+initializer 'locale.rb', <<-CODE
+  # frozen_string_literal: true
+
+  Rails.application.config.i18n do |i18n|
+    i18n.default_locale = :ja
+    i18n.available_locales = [:ja, :en]
+    i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+  end
+CODE
+
+initializer 'rack_lineprof.rb', <<-CODE
+  # frozen_string_literal: true
+
+  Rails.application.config.middleware.use Rack::Lineprof if Rails.env.development?
+CODE
+
 file '.rubocop.yml', <<-CODE
   require: rubocop-rails
+
+  AllCops:
+    Exclude:
+      - 'db/**/*'
+      - 'vendor/**/*'
+      - 'node_modules/**/*'
+      - 'tmp/**/*'
+      - 'bin/*'
+    DisplayCopNames: true
 
   Rails/UnknownEnv:
     Environments:
@@ -98,16 +123,40 @@ file '.rubocop.yml', <<-CODE
   Style/Documentation:
     Enabled: false
 
+  Style/TrailingCommaInHashLiteral:
+    EnforcedStyleForMultiline: comma
+
   Metrics/LineLength:
     Max: 120
+    Exclude:
+      - 'app/views/**/*'
+
+  Layout/MultilineMethodCallIndentation:
+    EnforcedStyle: indented
+
+  Lint/AmbiguousBlockAssociation:
+    Exclude:
+      - 'spec/**/*'
+
+  Metrics/BlockLength:
+    Exclude:
+      - 'spec/**/*'
 
   Metrics/MethodLength:
-    Max: 20
+    Max: 25
 CODE
+
+file 'db/seeds.rb', <<-CODE
+  SeedFu.seed
+CODE
+file 'db/fixtures/development/.keep'
 
 after_bundle do
   generate 'annotate:install'
   generate 'rspec:install'
   run 'bundle exec erb2slim app/views app/views -d'
   run 'bundle exec rubocop --auto-gen-config'
+  run 'bundle exec spring binstub rspec'
+  run 'bundle exec spring binstub rubocop'
+  run 'bin/spring stop'
 end
